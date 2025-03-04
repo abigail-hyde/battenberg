43e32f7c6d207e20e240bade95c5f8fccf7de805
Abi Hyde <77778322+abigail-hyde@users.noreply.github.com>
2025-03-04 14:47:45 +0000

    comment out ASCAT break

--- R/prepare_wgs.R
+++ R/prepare_wgs.R
@@ -154,16 +154,16 @@ getBAFsAndLogRs = function(tumourAlleleCountsFile.prefix, normalAlleleCountsFile
     	ch[[i]] = temp[1]:temp[length(temp)]
     }
   }
-  cat("checkpoint 2.6")
-  message("checkpoint 2.6")
-  ascat.bc = list(Tumor_LogR=as.data.frame(tumor.LogR[,3]), Tumor_BAF=as.data.frame(tumor.BAF[,3]),
-                  Germline_LogR=as.data.frame(germline.LogR[,3]), Germline_BAF=as.data.frame(germline.BAF[,3]),
-                  Tumor_LogR_segmented=NULL, Tumor_BAF_segmented=NULL, Tumor_counts=NULL, Germline_counts=NULL,
-                  SNPpos=tumor.LogR[,1:2], chrs=chr_names, samples=c(samplename), chrom=split_genome(tumor.LogR[,1:2]),
-                  ch=ch)
-  cat("checkpoint 2.7")
-  message("checkpoint 2.7")
-  ASCAT::ascat.plotRawData(ascat.bc) #, parentDir=figuresFile.prefix)
+  #cat("checkpoint 2.6")
+  #message("checkpoint 2.6")
+  #ascat.bc = list(Tumor_LogR=as.data.frame(tumor.LogR[,3]), Tumor_BAF=as.data.frame(tumor.BAF[,3]),
+  #                Germline_LogR=as.data.frame(germline.LogR[,3]), Germline_BAF=as.data.frame(germline.BAF[,3]),
+  #                Tumor_LogR_segmented=NULL, Tumor_BAF_segmented=NULL, Tumor_counts=NULL, Germline_counts=NULL,
+  #                SNPpos=tumor.LogR[,1:2], chrs=chr_names, samples=c(samplename), chrom=split_genome(tumor.LogR[,1:2]),
+  #                ch=ch)
+  #cat("checkpoint 2.7")
+  #message("checkpoint 2.7")
+  #ASCAT::ascat.plotRawData(ascat.bc) #, parentDir=figuresFile.prefix)
 }
 
 #' Prepare data for impute
